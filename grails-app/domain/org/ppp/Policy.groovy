07d4dcdbc4bfc63e2934f3eb4dc3206277274ba9
neodevelop <neodevelop@gmail.com>
2016-06-30 11:30:07 -0500

    #4 Adding constraints

--- grails-app/domain/org/ppp/Policy.groovy
+++ grails-app/domain/org/ppp/Policy.groovy
@@ -1,5 +1,6 @@
 package org.ppp
 
+// TODO: Separate in different classes
 class Policy {
 
   String registryNumber
@@ -45,7 +46,7 @@ class Policy {
   String stakeholdersNGOs
   String stakeholdersOthers
   String partners
-  String evidence_Strong_theory_of_change
+  String evidenceStrongTheoryOfChange
   String processMapAvailable
   String implementorsAvailableForComment
   String authors
@@ -62,5 +63,64 @@ class Policy {
   String analysisType
 
   static constraints = {
+    registryNumber(size:255)
+    registryCode(size:255)
+    policyAreaPP(size:255)
+    policySubcategory(size:255)
+    title(size:255)
+    theAbstract(size:1000)
+    tags(size:255)
+    country(size:255)
+    state(size:255)
+    countyMunicipality(size:255)
+    cityMetropolitanArea(size:255)
+    language(size:255)
+    targetedPopulation(size:255)
+    populationAffectedByTheProgram(size:255)
+    budget(size:255)
+    fundingDetails(size:255)
+    sponsors(size:300)
+    timeBetweenEnactmentAndFunding(size:255)
+    reasonForEnactment(size:1000)
+    problem(size:1000)
+    desiredOutcome(size:1000)
+    outcomes(size:1000)
+    evidenceQuality(size:1000)
+    linkToExternalEvaluation(size:500)
+    linkToPolicyDocuments(size:500)
+    linkToOtherDocumentationAboutThePolicy(size:500)
+    evaluationBased(size:1000)
+    dateOfEnactment(size:255)
+    dateOfImplementation(size:255)
+    dateOfClosure(size:255)
+    durationOfProgram(size:255)
+    legislativeReformsActs(size:1000)
+    totalAnnualOperationalCosts(size:255)
+    staffCosts(size:255)
+    transferCosts(size:255)
+    materialCosts(size:255)
+    otherCosts(size:255)
+    implementationTeamSizeNumber(size:1000)
+    stakeholdersPolicyMakers(size:1000)
+    stakeholdersCongressmen(size:1000)
+    stakeholdersNGOs(size:1000)
+    stakeholdersOthers(size:1000)
+    partners(size:1000)
+    evidenceStrongTheoryOfChange(size:500)
+    processMapAvailable(size:500)
+    implementorsAvailableForComment(size:300)
+    authors(size:1000)
+    authorsRelatedWork(size:300)
+    linkToAuthorsRelatedWork(size:300)
+    relatedPolicies(size:255)
+    released_by(size:500)
+    linkToReleasedEntity(size:100)
+    fileType(size:10)
+    fileCode(size:100)
+    fileFormat(size:255)
+    evaluatedBy(size:500)
+    linkToEvalutionEntity(size:100)
+    analysisType(size:100)
+
   }
 }
