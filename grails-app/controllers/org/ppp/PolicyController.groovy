94a585e4b0ca664267615718cef65d24dc97d0d3
neodevelop <neodevelop@gmail.com>
2016-06-30 10:51:30 -0500

    #4 adding domain class and controller

---
+++ grails-app/controllers/org/ppp/PolicyController.groovy
@@ -0,0 +1,7 @@
+package org.ppp
+
+class PolicyController {
+
+  static scaffold = Policiy
+
+}
---
+++ grails-app/domain/org/ppp/Policy.groovy
@@ -0,0 +1,66 @@
+package org.ppp
+
+class Policiy {
+
+  String registryNumber
+  String registryCode
+  String policyAreaPP
+  String policySubcategory
+  String title
+  String theAbstract
+  String tags
+  String country
+  String state
+  String countyMunicipality
+  String cityMetropolitanArea
+  String language
+  String targetedPopulation
+  String populationAffectedByTheProgram
+  String budget
+  String fundingDetails
+  String sponsors
+  String timeBetweenEnactmentAndFunding
+  String reasonForEnactment
+  String problem
+  String desiredOutcome
+  String outcomes
+  String evidenceQuality
+  String linkToExternalEvaluation
+  String linkToPolicyDocuments
+  String linkToOtherDocumentationAboutThePolicy
+  String evaluationBased
+  String dateOfEnactment
+  String dateOfImplementation
+  String dateOfClosure
+  String durationOfProgram
+  String legislativeReformsActs
+  String totalAnnualOperationalCosts
+  String staffCosts
+  String transferCosts
+  String materialCosts
+  String otherCosts
+  String implementationTeamSizeNumber
+  String stakeholdersPolicyMakers
+  String stakeholdersCongressmen
+  String stakeholdersNGOs
+  String stakeholdersOthers
+  String partners
+  String evidence_Strong_theory_of_change
+  String processMapAvailable
+  String implementorsAvailableForComment
+  String authors
+  String authorsRelatedWork
+  String linkToAuthorsRelatedWork
+  String relatedPolicies
+  String released_by
+  String linkToReleasedEntity
+  String fileType
+  String fileCode
+  String fileFormat
+  String evaluatedBy
+  String linkToEvalutionEntity
+  String analysisType
+
+  static constraints = {
+  }
+}
