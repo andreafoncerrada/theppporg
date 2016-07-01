febc00eb8b1f7760a789e8a859252de1fa673182
says33 <says.rodriguez@gmail.com>
2016-06-30 22:58:21 -0500

    #9 se agrega el metodo para el proceso de lectura de archivo

--- grails-app/controllers/org/ppp/PolicyController.groovy
+++ grails-app/controllers/org/ppp/PolicyController.groovy
@@ -1,7 +1,41 @@
 package org.ppp
 
+import jxl.Sheet
+import jxl.Workbook
+
+
 class PolicyController {
 
   static scaffold = Policy
+
+  def policyService
+
+  def upload() { }
+
+  def doUpload() {
+    def file = request.getFile('file')
+    Workbook workbook = Workbook.getWorkbook(file.getInputStream());
+   int numRows
+    (0..workbook.numberOfSheets-1).each{ sheetNo ->
+      Sheet page = workbook.getSheet(sheetNo)
+      int numColumns = page.columns
+      numRows = page.rows
+      String data
+      (1..numRows-1).each { row ->
+        def listElementInRow = []
+        (0..57).each { column ->
+          data = page.getCell(column, row).contents
+          listElementInRow.add(data)
+        }
+        policyService.createPolicyEntity(listElementInRow)
+        println 1
+      }
+    }
+    redirect (action:'list')
+  }
+
+  def list() {
+    render view: "list", model:[policyities: Policy.getAll()]
+  }
 
 }
