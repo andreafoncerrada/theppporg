475c693fb0dd07993557ddb568977afc6bb5818c
neodevelop <neodevelop@gmail.com>
2016-06-30 22:36:30 -0500

    #7 adding the search methods

--- grails-app/services/org/ppp/SearchService.groovy
+++ grails-app/services/org/ppp/SearchService.groovy
@@ -1,14 +1,21 @@
 package org.ppp
 
 import grails.transaction.Transactional
+import wslite.rest.RESTClient
 
 @Transactional
 class SearchService {
 
-  def addPolicy(Policy policy) {
+  RESTClient restClient
 
+  def addPolicy(Policy policy) {
+    def response = restClient.post(path:'/policy'){
+    }
+    response.json
   }
 
   def queryFor(String q){
+    def response = restClient.get(path:'/blog/post/_search', query:[query:"dilbert", pretty:true])
+    response.json
   }
 }
